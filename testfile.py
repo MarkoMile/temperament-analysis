8272d2733c401b3ff25aa8f51367e5919662e2e0
Marko <mmarkomile@gmail.com>
2022-09-02 12:18:25 +0200

    RandomForestClassifier with 0.37 accuracy

--- testfile.py
+++
@@ -1,6 +0,0 @@
-from sklearn.preprocessing import OneHotEncoder
-
-enc = OneHotEncoder()
-X = [['male', 'from US', 'uses Safari','female', 'that is', 'from Europe', 'uses Firefox']]
-enc.fit(X)
-print(enc.transform([['female', 'from US', 'uses Safari'],['male', 'from Europe', 'uses Safari']]).toarray())
